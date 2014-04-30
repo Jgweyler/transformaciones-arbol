29e487c68881f11a0b14fdd1dad8400c053d78dd
victor <victor@victor-VirtualBox.(none)>
2014-04-30 10:45:00 +0100

    Modificado el id y el secreto de google

--- auth.rb
+++ auth.rb
@@ -7,7 +7,7 @@ use OmniAuth::Builder do
   provider :google_oauth2, config['identifier'], config['secret']
 end
 
-#use omniAuth::Builder do
+#use OmniAuth::Builder do
 #  config = YAML.load_file 'config/config_templatett.yml'
 #  provider :twitter, config['identifier'], config['secret']
 #end
