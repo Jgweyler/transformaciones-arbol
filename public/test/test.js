e961f615e27d7d4b2314866efd6bdee9e3df7ec8
Jorge Gomez Weyler <alu0100717723@ull.edu.es>
2014-05-16 13:31:41 +0100

    solucionando problemas de tests

--- public/test/test.js
+++ public/test/test.js
@@ -2,7 +2,7 @@ var assert = chai.assert;
 
 suite('Pruebas', function(){
   test('Suma', function(){
-    obj = transformacion_pl0(pl0.parse("VAR x; x = 5 + 2 ."))
+    obj = pl0.parse("VAR x; x = 5 + 2 .")
     assert.equal(obj[1].right, "7")
   });
 
